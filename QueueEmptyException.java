57eb1cc29b38ce8ca313a91cf732ab688e3ce141
ibrahim <ibrahim@ibrahims-MacBook-Pro-2.local>
2021-09-19 13:24:31 +0100

    User defined Queue Empty Exception added

---
+++ QueueEmptyException.java
@@ -0,0 +1,11 @@
+// A user Defined Exception for Queue Full error
+
+public class QueueEmptyException extends Exception{
+
+	//@Override
+	public String toString(){
+		return "\nQueue is Empty"
+	}
+}
+
+
